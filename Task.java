5db81b948d42fab2e6d6e10ab3a97084a606f14a
nybbble <metinbbusra@gmail.com>
2025-09-19 00:43:03 +0300

    Create Task.java

---
+++ Task.java
@@ -0,0 +1,18 @@
+package com.example.taskmanagement.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private boolean completed;
+}
